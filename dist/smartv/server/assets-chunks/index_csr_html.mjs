83959cf2cd22aff62bf2cc7e53f9ab9d7bc613e6
smartv2024 <s85211022@gmail.com>
2025-02-02 22:10:51 +0100

    base url changed

--- dist/smartv/server/assets-chunks/index_csr_html.mjs
+++ dist/smartv/server/assets-chunks/index_csr_html.mjs
@@ -14,6 +14,6 @@ export default `<!doctype html>
   <app-root></app-root>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<link rel="modulepreload" href="chunk-3SBVSYCC.js"><script src="polyfills-EQXJKH7W.js" type="module"></script><script src="main-24SP2I2I.js" type="module"></script></body>
+<link rel="modulepreload" href="chunk-L2642ATL.js"><script src="polyfills-EQXJKH7W.js" type="module"></script><script src="main-BHKDAJSR.js" type="module"></script></body>
 </html>
 `;
