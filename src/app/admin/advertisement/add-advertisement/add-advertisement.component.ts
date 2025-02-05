cfcbfd95a5583be5b047ec2927a98704b537cae1
smartv2024 <s85211022@gmail.com>
2025-02-05 13:27:10 +0100

    comment removed

--- src/app/admin/advertisement/add-advertisement/add-advertisement.component.ts
+++ src/app/admin/advertisement/add-advertisement/add-advertisement.component.ts
@@ -24,45 +24,8 @@ export class AddAdvertisementComponent {
   videoResolutionError = ''; // To display resolution errors
 
   constructor(private adminService: AdminService, private router: Router) {}
-  validateVideoResolution(event: Event): void {
-    const inputElement = event.target as HTMLInputElement;
-    const videoUrl = inputElement.value;
-    this.videoResolutionError = ''; // Reset previous error message
   
-    if (videoUrl) {
-      this.getMediaDimensions(videoUrl)
-        .then((dimensions) => {
-          console.log('Video Dimensions:', dimensions);
-          if (dimensions.width > 1920 || dimensions.height > 1080) {
-            this.videoResolutionError =
-              'The video resolution exceeds the maximum allowed size of 1920x1080.';
-            this.advertisementObject.videoUrl = ''; // Clear the invalid URL
-          }
-        })
-        .catch((error) => {
-          console.error('Error fetching video dimensions:', error);
-          this.videoResolutionError =
-            'Unable to validate video resolution. Please provide a valid video URL.';
-          this.advertisementObject.videoUrl = ''; // Clear the invalid URL
-        });
-    }
-  }
-  
-  getMediaDimensions(videoUrl: string): Promise<{ width: number; height: number }> {
-    return new Promise((resolve, reject) => {
-      const video = document.createElement('video');
-      video.src = videoUrl;
-  
-      video.onloadedmetadata = () => {
-        resolve({ width: video.videoWidth, height: video.videoHeight });
-      };
-  
-      video.onerror = () => {
-        reject('Failed to load video metadata.');
-      };
-    });
-  }
-  
+ 
   addAdvertisement() {
     this.isSubmitting = true;
     this.successMessage = ''; // Clear previous success message
