375868497b7fcdcf3fd8b1561f9528d2c25b8994
smartv2024 <s85211022@gmail.com>
2025-05-17 15:47:08 +0100

    prod

--- src/environnement/enivronement.ts
+++ src/environnement/enivronement.ts
@@ -2,6 +2,6 @@ export const environment = {
    
 //baseUrl: 'https://tvad.onrender.com', // URL de production
 
- baseUrl: 'http://57.129.128.102:3002', // URL de développement
+ baseUrl: 'http://57.129.128.102:3001', // URL de développement
 //baseUrl: 'http://localhost:3000' // Socket.IO server URL
   };
